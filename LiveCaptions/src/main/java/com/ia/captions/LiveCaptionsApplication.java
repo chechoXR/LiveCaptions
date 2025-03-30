e73a1e636a331a519c6e9f9e3bf47714ea35e8a1
Sergio Ramirez <sergioestebanramirez@hotmail.com>
2025-03-30 13:01:24 -0500

    Implemented JavaFX and Logging

--- LiveCaptions/src/main/java/com/ia/captions/LiveCaptionsApplication.java
+++ LiveCaptions/src/main/java/com/ia/captions/LiveCaptionsApplication.java
@@ -1,14 +1,33 @@
 package com.ia.captions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 @SpringBootApplication
-public class LiveCaptionsApplication {
+public class LiveCaptionsApplication extends Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(LiveCaptionsApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Starting Live Captions App...");
 		SpringApplication.run(LiveCaptionsApplication.class, args);
-		System.out.println("Hello World!");
+		logger.info("Spring App started");
+		logger.info("Starting UI...");
+		Application.launch(args);
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.show();
+		logger.info("UI Started");
 	}
 
 }
