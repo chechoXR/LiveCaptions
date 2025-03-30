28d5907daf6bfb87157721d960a0a3dcb6dc09c0
Sergio Ramirez <sergioestebanramirez@hotmail.com>
2025-03-30 11:55:31 -0500

    Project created with hello world

---
+++ LiveCaptions/src/main/java/com/ia/captions/LiveCaptionsApplication.java
@@ -0,0 +1,14 @@
+package com.ia.captions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveCaptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveCaptionsApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ LiveCaptions/src/test/java/com/ia/captions/LiveCaptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ia.captions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveCaptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
